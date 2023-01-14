61d98ac2ce5c34aac56ba3c72a6c05f2bd121591
Cat Hanbit <tlsgus92wns@naver.com>
2023-01-14 23:13:03 +0900

    converter commit

--- src/main/java/hello/typeconverter/TypeconverterApplication.java
+++ src/main/java/hello/typeconverter/TypeconverterApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TypeconverterApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(TypeconverterApplication.class, args);
 	}
